fab0f9a448334c89ec06297d84a0d161c3669fe1
GatienDuh <78294761+GatienDuh@users.noreply.github.com>
2022-01-31 11:21:24 +0100

    ANIMATION DONE WOW + deplacement fleches working + deplacement ZQSD working

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,18 +10,23 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float jumpSpeed = 8.0f;
     [SerializeField] private float gravity = 20.0f;
 
+    private Animator anim;
+
     private Vector3 moveDirection = Vector3.zero;
 
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
 
     //#Movement
 
     private void Update()
     {
+        anim.SetFloat("Speed", Mathf.Abs(moveDirection.x));
+
         if (characterController.isGrounded)
         {
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
@@ -34,14 +39,14 @@ public class PlayerMovement : MonoBehaviour
         }
         moveDirection.y -= gravity * Time.deltaTime;
 
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
             Vector3 temp = transform.rotation.eulerAngles;
             temp.y = 0f;
             transform.rotation = Quaternion.Euler(temp);
         }
 
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
             Vector3 temp = transform.rotation.eulerAngles;
             temp.y = 180f;
